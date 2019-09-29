76da0dbc62b502a55d8927ca984dd7235d3fc5e5
jeremy <jeremygonzalez@outlook.com>
2019-09-29 08:32:33 -0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './basset.svg';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Basset example
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://basset.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more about basset
         </a>
       </header>
     </div>
